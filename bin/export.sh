8af42e0524827b924be9e31bc57f237e23c732f5
Bryan J. Brown <bjbrown@fsu.edu>
2019-06-28 15:10:20 -0400

    Adds enabled modules

--- bin/export.sh
+++ bin/export.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+rm /root/ldbase_config/sync/*
+
 cd /var/www/html/drupal
-/var/www/html/drupal/vendor/bin/drupal config:export --directory="/root/ldbase_config/sync/"
+/var/www/html/drupal/vendor/bin/drupal config:export --directory="/root/ldbase_config/sync/" --remove-config-hash
