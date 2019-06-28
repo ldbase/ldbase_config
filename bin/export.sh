d480923c5fdde2cf29e679797d8fc7c6d570bb65
Bryan J. Brown <bjbrown@fsu.edu>
2019-06-27 21:35:33 -0400

    Removes double slashes in drupal binary path

--- bin/export.sh
+++ bin/export.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd /var/www/html/drupal
-/var/www/html/drupal/vendor/bin//drupal config:export --directory="/root/ldbase_config/sync/"
+/var/www/html/drupal/vendor/bin/drupal config:export --directory="/root/ldbase_config/sync/"
